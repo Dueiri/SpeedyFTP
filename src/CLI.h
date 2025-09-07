f0fa20f554cceb7ea9a38efa106bc190f3ee2840
David <david.dueiri@outlook.com>
2025-09-07 13:49:48 -0700

    hello this is a commit

--- src/CLI.h
+++ src/CLI.h
@@ -13,39 +13,33 @@ David Dueiri
 class CLI_parser{
 
     public:
-    /**
-     * @brief Parses the user's command line input.
-     * 
-     * This function tokenizes the input string to identify the command
-     * and its arguments.
-     * 
-     * @param inputValue The raw string input from the user. It will be modified.
-     * @param cmd A pointer to a char that will contain the cmd code
-     * @param args A pointer to a std::vector that contains args that may be accessed in the future by other parts of the cli.
-     * @return a vector of chars which can be accessed by other modules.
-     */
-    void parseInputs(std::string& inputValue, char* cmd, std::vector<char>* args)
-    {
-        
-        std::stringstream ss(inputValue);
-        std::string token;
-        std::vector<std::string> tokens;
-        while (ss >> token) {
-            tokens.push_back(token);
-        }
-    private:
-    
-    std::vector<std::string> tokenizeInput(std::string cmdString)
+        /**
+         * @brief Parses the user's command line input.
+         * 
+         * This function tokenizes the input string to identify the command
+         * and its arguments.
+         * 
+         * @param inputValue The raw string input from the user. It will be modified.
+         * @param cmd A pointer to a char that will contain the cmd code
+         * @param args A pointer to a std::vector that contains args that may be accessed in the future by other parts of the cli.
+         * @return a vector of chars which can be accessed by other modules.
+         */
+        void parseInputs(std::string& inputValue, char* cmd, std::vector<char>* args)
         {
-            std::stringstream ss(cmdString);
-            std::string token;
-            std::vector<std::string> tokens;
-            while (ss >> token) {
-                tokens.push_back(token);
+            
             }
-            return tokens;
-        }
-        
+    private:
+        std::vector<std::string> tokenizeInput(std::string cmdString)
+            {
+                std::stringstream ss(cmdString);
+                std::string token;
+                std::vector<std::string> tokens;
+                while (ss >> token) {
+                    tokens.push_back(token);
+                }
+                return tokens;
+            }
+            
         //tokenize string
 
 
@@ -54,6 +48,3 @@ class CLI_parser{
 
 
     };
-};
-
-};
